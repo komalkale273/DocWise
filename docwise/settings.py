b36a60bafa237636bfa1fb1ffe2d4b021ed551f8
pranayc <pranaychandrikapure@gmail.com>
2026-06-09 23:47:05 +0530

    Fix

--- docwise/settings.py
+++ docwise/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-reu1x)bzr$2ftr%6p!poxfo)&1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
